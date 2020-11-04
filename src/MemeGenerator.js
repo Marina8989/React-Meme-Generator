d648dfdb7fc79143438d213a4fd4682a9aa6cb8a
Marina8989 <marina.mill209@gmail.com>
2020-11-03 20:51:18 -0800

    react meme generator

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,13 +3,40 @@ import React from 'react'
 class MemeGenerator extends React.Component{
     constructor() {
         super()
-        this.state={}
+        this.state={
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImg: []
+        }
     }
 
     render() {
         return (
             <div>
-                
+              <form onSubmit={this.submitChange}>
+                  <input 
+                    type="text"
+                    name="topText" 
+                    placeholder="Top Text" 
+                    value={this.state.topText} 
+                    onChange={this.handleChange}
+                   />
+                  <input
+                    type="text"
+                    name="bottomText"
+                    placeholder="Bottom Text"
+                    value={this.state.bottomText}
+                    onChange={this.handleChange}
+                   />
+
+                  <button>Generate</button>
+              </form>
+                <div>
+                    <img src={this.state.randomImg} />
+                    <h2>{this.state.topText}</h2>
+                    <h2>{this.state.bottomText}</h2>
+                </div>
             </div>
         )
     }
